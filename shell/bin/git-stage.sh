0929f60955acb00d129a8c62c69cd4a6e0e5ba53
sang <sang.chen@outlook.com>
2025-12-05 11:05:46 +0800

    feat(shell): Add delete option to git-stage.sh
    - Added "Delete" action to allow removing files from index
    - Updated gum choose menu to include delete option
    - Implemented git rm -r --cached for deleted files

--- shell/bin/git-stage.sh
+++ shell/bin/git-stage.sh
@@ -2,11 +2,14 @@
 
 ADD="Add"
 RESET="Reset"
+DELETE="Delete"
 
-ACTION=$(gum choose "$ADD" "$RESET")
+ACTION=$(gum choose "$ADD" "$RESET" "$DELETE")
 
 if [ "$ACTION" == "$ADD" ]; then
   git status --short | cut -c 4- | gum choose --no-limit | xargs git add
+elif [ "$ACTION" == "$DELETE" ]; then
+  git status --short | cut -c 4- | gum choose --no-limit | xargs git rm -r --cached
 else
   git status --short | cut -c 4- | gum choose --no-limit | xargs git restore
 fi
